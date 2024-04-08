cd7026c20356ba22d7e6941e516984ac72a74681
thyssem <thyssem.bazizi-makhlouf@epitech.eu>
2024-04-08 22:19:34 +0200

    [ADD] add caroussel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,73 @@
-import React, { Suspense } from 'react';
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
 import { Environment, OrbitControls } from '@react-three/drei';
-import Scene from '../public/Scene.jsx';
+import Scene from "../public/brain/Scene.jsx";
+import Kidney from "../public/kidney/Kidney.jsx";
 import './index.css';
 
-function App() {
+function CustomPrevArrow(props) {
+    const { className, onClick } = props;
     return (
-        <div>
-            <Canvas className="canvas">
-                <Suspense fallback={null}>
-                    <OrbitControls enableZoom={true} />
-                    <ambientLight />
-                    <Scene />
-                </Suspense>
-                <Environment preset="sunset"/>
-                <OrbitControls />
-            </Canvas>
-            <div className="info">
-                <h1>brain</h1>
-                <p>the brain is a complex organ that controls thought, memory, emotion, touch, motor skills, vision, breathing, temperature,
-                    hunger and every process that regulates our body. Together, the brain and spinal cord that extends from it make up the central nervous system, or CNS.</p>
+        <div className={className} onClick={onClick}>
+        </div>
+    );
+}
+
+function CustomNextArrow(props) {
+    const { className, onClick } = props;
+    return (
+        <div className={className} onClick={onClick}>
+        </div>
+    );
+}
+
+function App() {
+    // éléments 3D à afficher dans le carrousel
+    const items = [
+        {
+            key: 'scene',
+            scene: <Scene />,
+        },
+        {
+            key: 'kidney',
+            scene: <Kidney />,
+        },
+    ];
+
+    // Configuration du carrousel
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: <CustomPrevArrow />,
+        nextArrow: <CustomNextArrow />,
+    };
+
+    return (
+        <div className="container">
+            <div className="center">
+                <Slider {...settings}>
+                    {items.map(item => (
+                        <div key={item.key}>
+                            <h1>{item.title}</h1>
+                            <Canvas className="canvas">
+                                <Suspense fallback={null}>
+                                    <OrbitControls enableZoom={true} />
+                                    <ambientLight />
+                                    {item.scene}
+                                </Suspense>
+                                <Environment preset="sunset"/>
+                                <OrbitControls />
+                            </Canvas>
+                        </div>
+                    ))}
+                </Slider>
             </div>
         </div>
     );
