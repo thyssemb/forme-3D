0ef04091c16247351a0007fe8a21c119642b73f5
thyssem <thyssem.bazizi-makhlouf@epitech.eu>
2024-04-08 22:18:57 +0200

    [ADD] add folder for brain model

---
+++ client/public/brain/Scene.jsx
@@ -0,0 +1,25 @@
+import React, { useRef } from 'react';
+import { useGLTF } from '@react-three/drei';
+
+export default function Model(props) {
+    const groupRef = useRef();
+    const { nodes, materials } = useGLTF('/brain/scene.gltf');
+
+    if (groupRef.current) {
+
+        groupRef.current.scale.set(3, 3, 3); // taille
+    }
+
+    return (
+        <group {...props} ref={groupRef} dispose={null}>
+            <mesh geometry={nodes.Brain_Part_01_BRAIN_TEXTURE_blinn2_0.geometry} material={materials.BRAIN_TEXTURE_blinn2} />
+            <mesh geometry={nodes.Brain_Part_02_BRAIN_TEXTURE_blinn2_0.geometry} material={materials.BRAIN_TEXTURE_blinn2} />
+            <mesh geometry={nodes.Brain_Part_03_BRAIN_TEXTURE_blinn2_0.geometry} material={materials.BRAIN_TEXTURE_blinn2} />
+            <mesh geometry={nodes.Brain_Part_04_BRAIN_TEXTURE_blinn2_0.geometry} material={materials.BRAIN_TEXTURE_blinn2} />
+            <mesh geometry={nodes.Brain_Part_05_BRAIN_TEXTURE_blinn2_0.geometry} material={materials.BRAIN_TEXTURE_blinn2} />
+            <mesh geometry={nodes.Brain_Part_06_BRAIN_TEXTURE_blinn2_0.geometry} material={materials.BRAIN_TEXTURE_blinn2} />
+        </group>
+    );
+}
+
+useGLTF.preload('/scene.gltf');
